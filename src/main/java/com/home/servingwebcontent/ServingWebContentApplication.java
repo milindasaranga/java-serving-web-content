44519426f30b9c297b180f693f6a03f6e8e9c2b9
Milinda Saranga <milindasaranga@Milindas-MacBook-Pro.local>
2020-08-01 15:31:24 +0400

    serving web content

---
+++ src/main/java/com/home/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.home.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
+
+}
